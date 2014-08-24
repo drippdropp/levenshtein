69f904f40d337730924aca3dfef13b994e40e374
Daniel Marvin <dmarvin@capterra.com>
2014-08-24 01:10:23 -0400

    tabs to spaces

--- Leven.java
+++ Leven.java
@@ -1,46 +1,46 @@
 public class Leven
 {
-	public static int Levenshtein(String s0, String s1)
-	{
-		int s0_len = s0.length() + 1;
-		int s1_len = s1.length() + 1;
+    public static int Levenshtein(String s0, String s1)
+    {
+        int s0_len = s0.length() + 1;
+        int s1_len = s1.length() + 1;
 
-		int[] cost = new int[s0_len];
-		int[] new_cost = new int[s0_len];
+        int[] cost = new int[s0_len];
+        int[] new_cost = new int[s0_len];
 
-		for (int i = 0; i < s0_len; i++)
-			cost[i] = i;
+        for (int i = 0; i < s0_len; i++)
+            cost[i] = i;
 
-		try {
+        try {
 
-			for (int j = 1; j < s1_len; j++)
-			{
-				new_cost[0] = j;
-				for (int i = 1; i < s0_len; i++)
-				{
-					int match = (s0.charAt(i-1) == s1.charAt(j-1)) ? 0 : 1;
+            for (int j = 1; j < s1_len; j++)
+            {
+                new_cost[0] = j;
+                for (int i = 1; i < s0_len; i++)
+                {
+                    int match = (s0.charAt(i-1) == s1.charAt(j-1)) ? 0 : 1;
 
-					int cost_replace = cost[i-1] + match;
-					int cost_insert = cost[i] + 1;
-					int cost_delete = new_cost[i-1] + 1;
+                    int cost_replace = cost[i-1] + match;
+                    int cost_insert = cost[i] + 1;
+                    int cost_delete = new_cost[i-1] + 1;
 
-					new_cost[i] = Math.min(
-						Math.min(
-							cost_insert, 
-							cost_delete), 
-						cost_replace);
-				}
-				int[] swap = cost; cost = new_cost; new_cost = swap;
-			}
+                    new_cost[i] = Math.min(
+                        Math.min(
+                            cost_insert, 
+                            cost_delete), 
+                        cost_replace);
+                }
+                int[] swap = cost; cost = new_cost; new_cost = swap;
+            }
 
-			return cost[s0_len-1];
+            return cost[s0_len-1];
 
-		} catch ()
+        } catch ()
 
-	}
+    }
 
-	public static void main(String[] args)
-	{
-		System.out.println(Levenshtein("aba", "acb"));
-	}
+    public static void main(String[] args)
+    {
+        System.out.println(Levenshtein("aba", "acb"));
+    }
 }
